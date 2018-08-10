f558de6faf005fda7461dcfcfdb662e286f9f583
Krzysztof Mokszan <krzysztof.mokszan@nielsen.com>
2018-08-10 06:31:13 -0700

    add public method descriptions and test for some edge cases

--- src/main/java/Combinations.java
+++ src/main/java/Combinations.java
@@ -1,12 +1,23 @@
 
 
+import com.google.common.base.Strings;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Combinations {
 
-    public static void combine(String str){
-        combineIter(new StringBuilder(str)).forEach(i-> System.out.println(i));
+    /**Task:
+     * This method prints out all combinations of all letters in a given string.
+     * To implement this method, please collect all combinations in a collection and print them out
+     * Assume only ASCII characters are present in a string
+     *
+     * @param str
+     */
+    public static void combineWithCollection(String str){
+        if(!Strings.isNullOrEmpty(str)){
+            combineIter(new StringBuilder(str)).forEach(i-> System.out.println(i));
+        }
     }
 
     private static List<String> combineIter(StringBuilder str){
@@ -18,7 +29,6 @@ public class Combinations {
             List<String> a = new ArrayList<>();
             char lastLetter = str.charAt(str.length()-1);
             a.add(lastLetter+"");
-            //StringBuilder sb = new StringBuilder(str);
             StringBuilder sb = str;
             sb.setLength(sb.length()-1);
             List<String> previousIter = combineIter(sb);
@@ -28,8 +38,16 @@ public class Combinations {
         }
     }
 
-    public static void combine2(String str){
-        combineIter2(new StringBuilder(str),0,new StringBuilder());
+    /**Task:
+     * This method prints out all combinations of all letters in a given string.
+     * Assume only ASCII characters are present in a string
+     *
+     * @param str
+     */
+    public static void combine(String str){
+        if(!Strings.isNullOrEmpty(str)){
+            combineIter2(new StringBuilder(str),0,new StringBuilder());
+        }
     }
 
     private static void combineIter2(StringBuilder in, int start, StringBuilder out){
@@ -45,7 +63,7 @@ public class Combinations {
         }
     }
 
-    public static Map<Integer, List<String>> m = new HashMap<>();
+    private static Map<Integer, List<String>> m = new HashMap<>();
     static {
         m.put(0, Arrays.asList("0"));
         m.put(1, Arrays.asList("1"));
@@ -53,6 +71,12 @@ public class Combinations {
         m.put(3, Arrays.asList("g","h","i"));
     }
 
+    /**
+     * Task:
+     * For a given phoneNumber, this method prints all combinations of associated letters to each number according to existing dictionary
+     * If a given list of number includes a null value or an unmapped integer, throw IllegalArgumentException
+     * @param phoneNumber
+     */
     public static void phoneNumberToLetters(List<Integer> phoneNumber){
         if(phoneNumber==null) throw new IllegalArgumentException("no phone number given");
         if(phoneNumber.size()>10) throw new IllegalArgumentException("too long number");
@@ -71,20 +95,32 @@ public class Combinations {
         }
     }
 
-    private static final List<String> EMPTY_LIST = new ArrayList<>();
-
     private static List<String> getMappedChars(List<Integer> phoneNumber, int index) {
         Integer number = phoneNumber.get(index);
-        if(number==null)return EMPTY_LIST;
-        return m.get(number) == null ? EMPTY_LIST : m.get(number);
+        if(number==null)throw new IllegalArgumentException(String.format("number at index %d was null",index));
+        List<String> result = m.get(number);
+        if(result == null) throw new IllegalArgumentException(String.format("number %d at index %d is invalid",number,index));
+        return result;
     }
 
 
     public static void main(String[] args) {
-//        combine("abc");
-//        System.out.println("--------------");
-//        combine2("abc");
-//        System.out.println("--------------");
+        combineWithCollection("abc");
+        System.out.println("--------------");
+        combine("abc");
+        System.out.println("--------------");
+        try{
+            phoneNumberToLetters(Arrays.asList(1,null,3));
+        }catch (IllegalArgumentException e){
+            System.out.println("expected exception was caught: " + e);
+        }
+        System.out.println("--------------");
+        try{
+            phoneNumberToLetters(Arrays.asList(1,-9,3));
+        }catch (IllegalArgumentException e){
+            System.out.println("expected exception was caught: " + e);
+        }
+        System.out.println("--------------");
         phoneNumberToLetters(Arrays.asList(1,2,3));
     }
 }
